e18c89f2e92bfef5e2c4c4dee8bdb15f3217bf31
Saad3 <sa.alsahli@gmail.com>
2016-10-16 03:52:23 +0300

    test location

--- src/finalProject/TestingMain.java
+++ src/finalProject/TestingMain.java
@@ -1,10 +1,33 @@
 package finalProject;
 
+import java.sql.*;
+
 public class TestingMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection connection;
+		Statement statement;
+		ResultSet result;
+		try {
+		String password="";
+		String username="root";
+		String url="jdbc:mysql://localhost:3306/offline?useUnicode=yes&characterEncoding=UTF-8";
+		connection = DriverManager.getConnection(url, username, password);
+		
+		statement = connection.createStatement();
+		String x = "SELECT * FROM user WHERE location LIKE '%الرياض%' ";
+		
+			PreparedStatement statement1 = connection.prepareStatement(x);
+		
+			result = statement1.executeQuery();
+		      while(result.next()){
+			System.out.println(result.getString("location"));
+		      }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 		
 	}
 
