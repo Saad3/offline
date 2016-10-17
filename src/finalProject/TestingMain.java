8d830aedbb4405d83e25ce9391d4005cd338771d
Saad3 <sa.alsahli@gmail.com>
2016-10-18 01:40:50 +0300

    update citys

--- src/finalProject/TestingMain.java
+++ src/finalProject/TestingMain.java
@@ -6,7 +6,6 @@ public class TestingMain {
 
 	public static void main(String[] args) {
 		Connection connection;
-		Statement statement;
 		ResultSet result;
 		try {
 		String password="";
@@ -14,7 +13,6 @@ public class TestingMain {
 		String url="jdbc:mysql://localhost:3306/offline?useUnicode=yes&characterEncoding=UTF-8";
 		connection = DriverManager.getConnection(url, username, password);
 		
-		statement = connection.createStatement();
 		String x = "SELECT * FROM user WHERE location LIKE '%الرياض%' ";
 		
 			PreparedStatement statement1 = connection.prepareStatement(x);
