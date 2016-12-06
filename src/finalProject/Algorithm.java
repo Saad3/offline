91841313575c1325d2521634a3a6bf43cce32d13
Saad3 <sa.alsahli@gmail.com>
2016-12-06 16:47:23 +0300

    update

--- src/finalProject/Algorithm.java
+++ src/finalProject/Algorithm.java
@@ -16,9 +16,12 @@ public class Algorithm {
 	private String optimized;
 	private String[] emotionWord;
 	private int[][] emotionWeight;
+	private int[][] originalEmotionWeight;
 	private Stack<String> tweet;
 	private int resultCounter;
-	private int positiveEmotion, negativeEmotion, positiveNum, negativeNum;
+	private int  positiveEmotion, negativeEmotion, positiveNum, negativeNum;
+	private String[][] city;
+
 
 	public Algorithm(String url, String username, String password, String offline, String optimized) {
 
@@ -44,71 +47,64 @@ public class Algorithm {
 
 		retrieveEmotion();
 		System.out.println("retrieveEmotion is Done!");
+		retrieveCityNames();
+		System.out.println("retrieveCityNames is Done!");
+		//calculateEmotionalScoreForCities();
+		//calculateEmotionalScore();
 		retrevtweets();
-		System.out.println("retrevtweets is Done!");
-		emotionFinder();
-		System.out.println("emotionFinder is Done!");
-		calculateEmotionalScore();
-		
+		emotionFinder() ;
+		calculateEmotionalScoreForOneCity();
 		System.out.println("Done !!!!!!!");
 	
 	}
 
-	public void algorithm() {
-
-		try {
-			int maximumIteration = 0;
-			int t = 0;
-			while (t <= maximumIteration) {
-				for (int U = 0; U < emotionWord.length; U++) {// U Sets of users.
-					for (int I = 0; I < emotionWord.length; I++) {// I Sets of items.
-						for (int T = 0; T < emotionWord.length; T++) {// T Sets of
-																	// tags.
-							calculateEmotionalScore();
-							calculateItemTagRelevance();
-						}
-					}
-
-				}
-
-				for (int U = 0; U < emotionWord.length; U++) {
-					calculateUserUserSimilarity();
-				}
-				for (int I = 0; I < emotionWord.length; I++) {
-					calculateItemItemSimilarity();
-				}
-				for (int T = 0; T < emotionWord.length; T++) {
-					calculateTagTagSimilarity();
-				}
-
-			}
-
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-
-	private void calculateEmotionalScore() {
-		
+	private void calculateEmotionalScoreForCities() { // you can't call this method before calling retrieveCityNames()
 		
 		try {
 			
+			for (int i = 0; i < city.length; i++) {
+				retrevtweets(city[i][0]);
+				calculateEmotionalScoreForOneCity(city[i][3]);
+			}
+			retrevtweets("NULL");
+			calculateEmotionalScoreForOneCity("unlocated");
+			
+		} catch (Exception e) {
+			System.out.println("error in calculateEmotionalScoreForCity()");
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	private void calculateEmotionalScoreForOneCity(String States) {
+		System.out.println("The Result for "+States);
+
+		
+		try {
+			int positiveEmotion, negativeEmotion, positiveNum, negativeNum;
+			positiveEmotion = negativeEmotion = positiveNum = negativeNum = 0;
+			int emotionWeight[][]= emotionFinder(originalEmotionWeight);
+			
 			for (int i = 0; i < emotionWeight.length; i++) {
 				
-				System.out.println("This Emotion: "+emotionWeight[i]+" "
+				System.out.println("This Emotion: "+emotionWord[i]+" "
 						+ "ID is: "+emotionWeight[i][0]+" "
 						+ "Weight is: "+emotionWeight[i][1]+" "
 						+ "Used: "+emotionWeight[i][2]+" Time "
 						+ "The Total Weight is: "+(emotionWeight[i][1]*emotionWeight[i][2]));
 				
 				if (emotionWeight[i][1] < 0) {
-					negativeNum=+emotionWeight[i][2];
-					negativeEmotion =+ emotionWeight[i][1];
+					negativeNum=negativeNum+emotionWeight[i][2];
+					negativeEmotion =negativeEmotion+ emotionWeight[i][1];
 				} else {
-					positiveNum=+emotionWeight[i][2];
-					positiveEmotion =+ emotionWeight[i][1];
+					positiveNum=positiveNum+emotionWeight[i][2];
+					positiveEmotion =positiveEmotion+ emotionWeight[i][1];
 				}
 			}//end of for
+			//TODO fix the problem
+			System.out.println("The Result for "+States+"is:");
 			
 			System.out.println("The Number of negative Emotions is: "+negativeNum+" "
 					+ "The Sum weight of negativity is: "+negativeEmotion);
@@ -120,7 +116,11 @@ public class Algorithm {
 					+ ((positiveEmotion*100)/(positiveEmotion+negativeEmotion))+"% Positive "
 							+ ((negativeEmotion*100)/(positiveEmotion+negativeEmotion))+"% Negative");
 			
-			
+			this.positiveEmotion = this.positiveEmotion + positiveEmotion;
+			this.negativeEmotion = this.negativeEmotion + negativeEmotion;
+			this.positiveNum = this.positiveNum + positiveNum;
+			this.negativeNum = this.negativeNum + negativeNum;
+
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -129,7 +129,7 @@ public class Algorithm {
 		
 	}
 
-	private void retrevtweets() {
+	private void retrevtweets(String states_id) {
 		try {
 			setResultCounter();
 
@@ -141,9 +141,13 @@ public class Algorithm {
 			while (i < resultCounter) {
 				PreparedStatement res;
 				res = connection
-						.prepareStatement("SELECT textual_content FROM " + optimized + ".item WHERE 1 limit ?, ?");
-				res.setInt(1, startNum);
-				res.setInt(2, endNum);
+						.prepareStatement("SELECT textual_content FROM " + optimized + ".item "
+										+ "INNER JOIN " + optimized + ".user "
+										+ "WHERE " + optimized + ".user.states_id =?"
+										+ "limit ?, ?");
+				res.setString(1, states_id);
+				res.setInt(2, startNum);
+				res.setInt(3, endNum);
 				result = res.executeQuery();
 				while (result.next()) {
 					tweet.push(result.getString("textual_content"));
@@ -182,27 +186,7 @@ public class Algorithm {
 		}
 	}
 
-	private void calculateTagTagSimilarity() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void calculateItemItemSimilarity() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void calculateUserUserSimilarity() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void calculateItemTagRelevance() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void emotionFinder() {
+	private int[][] emotionFinder(int cityEmotionWeight[][]) {
 		
 		try {
 			
@@ -211,16 +195,8 @@ public class Algorithm {
 				testedTweet = tweet.pop();
 				for (int i = 0; i < emotionWord.length; i++) {
 					if (testedTweet.contains(emotionWord[i])) {
-						emotionWeight[i][2]++;//may not work you should check the default value
-																// int
-					/*	if (temp < 0) {
-							negativeNum++;
-							negativeemotion = negativeemotion + temp;
-						} else {
-							positiveNum++;
-							positiveemotion = positiveemotion + temp;
-						}*/
-
+						emotionWeight[i][2]++;
+						cityEmotionWeight[i][2]++;
 					}
 				}
 			}
@@ -229,6 +205,7 @@ public class Algorithm {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return cityEmotionWeight;
 	}
 
 	private boolean retrieveEmotion() {
@@ -250,6 +227,7 @@ public class Algorithm {
 				emotionWeight[i][0] = result.getInt("keyword_id");
 				emotionWeight[i][1] = result.getInt("weight");
 			}
+			originalEmotionWeight=emotionWeight;
 
 			return true;
 
@@ -258,5 +236,214 @@ public class Algorithm {
 			return false;
 		}
 	}
+	
+	private void retrieveCityNames() {
+		try {
 
+			PreparedStatement countQuery, retrieveQuery;
+			retrieveQuery = connection.prepareStatement("SELECT * FROM " + optimized + ".cities WHERE " +optimized+ ".cities.parent_id < 14");//TODO Fix
+			countQuery = connection.prepareStatement("SELECT COUNT(*)AS total FROM " + optimized + ".cities ");
+			result = countQuery.executeQuery();
+			result.next();
+			city = new String[result.getInt("total")][4];
+
+			result = retrieveQuery.executeQuery();
+
+			// 0 will hold states number 1 will hold city name in arabic 2 in english
+			//while (result.next()) {
+				
+				for (int i = 0; i < city.length; i++) {
+					result.next();
+					city[i][0] = result.getString("id");
+					city[i][1] = result.getString("parent_id");
+					city[i][2] = result.getString("arabic");
+					city[i][3] = result.getString("english");
+					
+
+				}
+			//}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
+	public void algorithm() {
+
+		try {
+			int maximumIteration = 0;
+			int t = 0;
+			while (t <= maximumIteration) {
+				for (int U = 0; U < emotionWord.length; U++) {// U Sets of users.
+					for (int I = 0; I < emotionWord.length; I++) {// I Sets of items.
+						for (int T = 0; T < emotionWord.length; T++) {// T Sets of
+																	// tags.
+						//	calculateEmotionalScore();
+							calculateItemTagRelevance();
+						}
+					}
+
+				}
+
+				for (int U = 0; U < emotionWord.length; U++) {
+					calculateUserUserSimilarity();
+				}
+				for (int I = 0; I < emotionWord.length; I++) {
+					calculateItemItemSimilarity();
+				}
+				for (int T = 0; T < emotionWord.length; T++) {
+					calculateTagTagSimilarity();
+				}
+
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private void calculateTagTagSimilarity() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void calculateItemItemSimilarity() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void calculateUserUserSimilarity() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void calculateItemTagRelevance() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+	
+	
+	
+	
+	//////////////////////////////////////////////
+	
+	private void retrevtweets() {
+		try {
+			setResultCounter();
+
+			int startNum, endNum, i;
+			i = 0;
+			startNum = 0;
+			endNum = 500;
+
+			while (i < resultCounter) {
+				PreparedStatement res;
+				res = connection
+						.prepareStatement("SELECT textual_content FROM " + optimized + ".item "
+										+ "limit ?, ?");
+				res.setInt(1, startNum);
+				res.setInt(2, endNum);
+				result = res.executeQuery();
+				while (result.next()) {
+					tweet.push(result.getString("textual_content"));
+					i++;
+					result.next();
+
+				}
+				startNum = endNum + 1;
+				if (endNum + 500 > resultCounter)
+					endNum = resultCounter;
+				else
+					endNum = +500;
+
+			}
+			System.out.println("the number of tweet is: " + resultCounter);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+	
+	private void emotionFinder() {
+		
+		try {
+			
+			String testedTweet = null;
+			while (!tweet.isEmpty()) {
+				testedTweet = tweet.pop();
+				for (int i = 0; i < emotionWord.length; i++) {
+					if (testedTweet.contains(emotionWord[i])) {
+						emotionWeight[i][2]++;
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void calculateEmotionalScoreForOneCity( ) {
+
+		
+		try {
+	
+			for (int i = 0; i < emotionWeight.length; i++) {
+				if (emotionWeight[i][2]==0){
+					continue;
+				}
+				
+				System.out.println("This Emotion: "+emotionWord[i]+" "
+						+ "ID is: "+emotionWeight[i][0]+" "
+						+ "Weight is: "+emotionWeight[i][1]+" "
+						+ "Used: "+emotionWeight[i][2]+" Time "
+						+ "The Total Weight is: "+(emotionWeight[i][1]*emotionWeight[i][2]));
+				
+				if (emotionWeight[i][1] < 0) {
+					negativeNum=negativeNum+emotionWeight[i][2];
+					negativeEmotion =negativeEmotion+ (emotionWeight[i][1]*emotionWeight[i][2]);
+				} else {
+					positiveNum=positiveNum+emotionWeight[i][2];
+					positiveEmotion =positiveEmotion+ (emotionWeight[i][1]*emotionWeight[i][2]);
+				}
+			}//end of for
+			//TODO fix the problem
+		
+
+			System.out.println("\nThe Total Number Emotions is: " + (positiveNum + negativeNum) + " | " 
+			+ "positive= "
+			+ positiveNum + " & Negative= " + negativeNum);
+			
+			System.out.println("\nthe total positive Emotion is: "+positiveEmotion+" "
+					+ "& the total negativeEmotion is: "+negativeEmotion);
+			
+			
+			double positive = (double)((positiveEmotion * 100) / ((double)(Math.abs(positiveEmotion) + Math.abs(negativeEmotion))));
+			double negative = ((double)(Math.abs(negativeEmotion) * 100) / (double)((Math.abs(positiveEmotion) +Math.abs(negativeEmotion))));
+
+			System.out.println("\n"+ Math.round(positive)+ "% Positive "
+					+ Math.rint(negative) + "% Negative");
+
+			positiveEmotion = positiveEmotion + positiveEmotion;
+			negativeEmotion = negativeEmotion + negativeEmotion;
+			positiveNum = positiveNum + positiveNum;
+			negativeNum = negativeNum + negativeNum;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
--- src/finalProject/Cleaner.java
+++ src/finalProject/Cleaner.java
@@ -1,6 +1,7 @@
 package finalProject;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Cleaner {
 
@@ -27,11 +28,36 @@ public class Cleaner {
 	}
 
 	public void startClean() {
-		retrieveCityNames();
-		if (city.length != 0) {
-		cleanCityName();
+		
+		try {
+			System.out.println("Enter 1 to find the city form the user pio or user location\n"
+					+ "Enter 2 to Start copying the Data to the optmized DB\n"
+					+ "Or Enter 0 to exit the Cleaner\n");
+			
+			Scanner input = new Scanner(System.in);
+			int key=0;
+			key = input.nextInt();
+			switch (key) {
+			case 1:
+				retrieveCityNames();
+				if (city.length != 0) {
+				cleanCityName();
+				System.out.println("Done !!");
+				}
+				break;
+			case 2:
+				copier();
+				break;
+			case 0:
+				break;
+					
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		copier();
+		
 	}
 	
 	private void copier(){
@@ -39,16 +65,49 @@ public class Cleaner {
 		try {
 			
 			PreparedStatement query;
-			query = connection.prepareStatement("INSERT INTO "+optimized+".user SELECT * FROM "+offline+".user_opt "); //copy user
+			query = connection.prepareStatement("INSERT INTO  "+optimized+".item "
+					+ "SELECT offline.item.item_id , offline.item.user_id , offline.item.textual_content ,offline.item.tag_count ,offline.item.timestamp "
+					+ "FROM offline.item WHERE ((offline.item.url_count < 2 ) and ("+offline+".item.tag_count < 4 )) "); //copy item
+			
 			query.executeUpdate();
-			query = connection.prepareStatement("INSERT INTO  "+optimized+".item SELECT * FROM "+offline+".item_opt"); //copy item
+			System.out.println("Done copying the item");
+			
+			query = connection.prepareStatement("INSERT INTO "+optimized+".user SELECT "+offline+".user.user_id ,"+offline+".user.city_id ,"+offline+".user.states_id "
+					+ "FROM "+offline+".user "); //copy user
 			query.executeUpdate();
-			query = connection.prepareStatement("INSERT INTO  "+optimized+".contain SELECT * FROM "+offline+".contain_opt"); //copy contain
+			System.out.println("Done copying the user");
+			
+			query = connection.prepareStatement("DELETE FROM  "+optimized+".user  WHERE "+optimized+".user.user_id NOT IN(SELECT "+optimized+".item.user_id FROM "+optimized+".item)"); //copy contain
+			//query.executeUpdate();
+			
+			System.out.println("Done cleanibn the user");
+			
+			
+			/*
+			INSERT INTO optimized_offline_db.user
+			SELECT DISTINCT offline.user.user_id , offline.user.city_id, offline.user.states_id
+			FROM offline.user
+			JOIN optimized_offline_db.item
+			ON offline.user.user_id=optimized_offline_db.item.user_id 
+			  
+			 
+			  */
+			
+			
+			/*	
+			query = connection.prepareStatement("INSERT INTO  "+optimized+".contain SELECT * FROM "+offline+".contain"
+					+ "WHERE "+offline+".contain.item_id IN (SELECT "+optimized+".item.item_id FROM "+optimized+".item)"); //copy contain
 			query.executeUpdate();
+			System.out.println("Done copying the contain");
+			
 			query = connection.prepareStatement("INSERT INTO  "+optimized+".tags SELECT * FROM "+offline+".tags"); //copy tag
 			query.executeUpdate();
+			System.out.println("Done copying the tags");
+			
 			query = connection.prepareStatement("INSERT INTO  "+optimized+".geotag SELECT * FROM "+offline+".geotag"); //copy tag
 			query.executeUpdate();
+			System.out.println("Done copying the geotag");*/
+
 		
 		
 		
@@ -60,9 +119,6 @@ public class Cleaner {
 		
 	}
 	
-	// item cleaner
-
-	// user cleaner
 	private void retrieveCityNames() {
 		try {
 
@@ -71,7 +127,7 @@ public class Cleaner {
 			countQuery = connection.prepareStatement("SELECT COUNT(*)AS total FROM `cities` ");
 			result = countQuery.executeQuery();
 			result.next();
-			city = new String[result.getInt("total")][3];
+			city = new String[result.getInt("total")][4];
 
 			result = retrieveQuery.executeQuery();
 
@@ -80,9 +136,11 @@ public class Cleaner {
 				
 				for (int i = 0; i < city.length; i++) {
 					result.next();
-					city[i][0] = result.getString("parent_id");
-					city[i][1] = result.getString("arabic");
-					city[i][2] = result.getString("english");
+					city[i][0] = result.getString("id");
+					city[i][1] = result.getString("parent_id");
+					city[i][2] = result.getString("arabic");
+					city[i][3] = result.getString("english");
+					
 
 				}
 			//}
@@ -100,11 +158,15 @@ public class Cleaner {
 			
 			PreparedStatement query;
 			for (int i = 0; i < city.length; i++) {
-				query = connection.prepareStatement("UPDATE "+offline+".user SET `city`=?,`states`=? WHERE (location LIKE ? OR location LIKE ?)");
-				query.setString(1,city[i][1]);
-				query.setString(2,city[i][0]);
-				query.setString(3,"%"+city[i][1]+"%");//one for English
-				query.setString(4,"%"+city[i][2]+"%");
+				query = connection.prepareStatement("UPDATE "+offline+".user SET `city_id`=?,`states_id`=? WHERE "
+						+ "(location LIKE ? OR location LIKE ? "
+						+ "OR user_pio LIKE ? OR user_pio LIKE ?)");
+				query.setString(1,city[i][0]);
+				query.setString(2,city[i][1]);
+				query.setString(3,"%"+city[i][2]+"%");//one for English
+				query.setString(4,"%"+city[i][3]+"%");
+				query.setString(5,"%"+city[i][2]+"%");//one for English
+				query.setString(6,"%"+city[i][3]+"%");
 				query.executeUpdate();
 
 			}
